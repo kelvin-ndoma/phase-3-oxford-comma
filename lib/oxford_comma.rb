12ffc69a08a54e77e2bdcc406169ec697cb8f45f
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-02-13 08:24:27 +0300

    completed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+# require 'pry'
 
+def oxford_comma(array)
+    if array.length < 3
+        array.join(' and ')
+    else
+        array[-1] = "and #{array[-1]}"
+        array.join(", ")
+    end
 end
+puts oxford_comma(["kelvin", "mutua"])
+puts oxford_comma(["kelvin", "mutua", "sfd3a"])
+puts oxford_comma(["kelvin", "mutua", "sfd3a", "student"])
+
+# binding.pry
